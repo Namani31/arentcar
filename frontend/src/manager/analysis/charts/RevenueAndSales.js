3d8632a87f96078cabaeb02c2b705a098464f904
Namani <kwg531@naver.com>
2024-11-19 00:48:12 +0900

    임시: <h3> 태그 적용 안 됨

--- frontend/src/manager/analysis/charts/RevenueAndSales.js
+++ frontend/src/manager/analysis/charts/RevenueAndSales.js
@@ -3,12 +3,16 @@ import React from 'react';
 import SalesChart from './SalesChart'; // 매출 차트
 import RevenueChart from './RevenueChart'; // 수익 차트
 
+// TODO: <h3> 태그가 왜 적용이 되지 않는지 알아볼 것
 const RevenueAndSales = ({ type }) => {
     return (
         <div>
-            {type === 'total' && <RevenueChart />} {/* 전체 매출 */}
-            {type === 'vehicle' && <SalesChart />} {/* 차량별 매출 */}
-            {type === 'period' && <SalesChart />} {/* 기간별 매출 */}
+            <div className="chart-container">
+                <h3>수익 밎 매출</h3>
+                {type === 'total' && <RevenueChart />} {/* 전체 매출 */}
+                {type === 'vehicle' && <SalesChart />} {/* 차량별 매출 */}
+                {type === 'period' && <SalesChart />} {/* 기간별 매출 */}
+            </div>
         </div>
     );
 };
