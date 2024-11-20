e487bd7c435ed16b949c5f53f10549e4bb5b8c0f
Namani <kwg531@naver.com>
2024-11-20 15:22:20 +0900

    Refactor: 등록된 없어진 컴포넌트들 삭제 반영

--- frontend/src/manager/analysis/AnalysisAndStatistics.js
+++ frontend/src/manager/analysis/AnalysisAndStatistics.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Dashboard from './charts/Dashboard';
 
 const AnalysisAndStatistics = () => {
   return (
     <div>
       <h1>This is analysis and statistics page.</h1>
-      <Dashboard />
     </div>
   );
 }
