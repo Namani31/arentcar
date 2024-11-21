31eb69bb461166e45ffc97150ec6f16dd93e148d
Namani <kwg531@naver.com>
2024-11-21 11:38:47 +0900

    Feat: `AllBranchesReservsationChart.js` 일별, 월별에 따라 제목 변하게 변경

--- frontend/src/manager/analysis/charts/AllBranchesReservationChart.js
+++ frontend/src/manager/analysis/charts/AllBranchesReservationChart.js
@@ -1,26 +1,35 @@
 // 지역별 예약 건수
 import React from 'react';
-import {Chart as ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend} from 'chart.js';
+import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 
 // 필요한 요소를 등록
 ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
-const AllBranchesReservationChart = () => {
+// filter 값을 props로 받음 (daily, monthly)
+const AllBranchesReservationChart = ({ filter }) => {
     const data = {
         labels: ['서울', '부산', '대구', '광주', '대전'],
         datasets: [
             {
-                label: '지역별 예약 건수',
+                label: '예약 건수',
                 data: [50, 30, 20, 40, 10],
                 backgroundColor: ['red', 'green', 'blue', 'yellow', 'purple'],
             },
         ],
     };
 
+    let filterText;
+
+    if (filter === 'daily') {
+        filterText = '일별 전체 지점 예약 통계';
+    } else {
+        filterText = '월별 전체 지점 예약 통계';
+    }
+
     return (
         <div className="chart-container">
-            <h3>일별 전체 지점별 예약 건수</h3>
+            <h3>{filterText}</h3>
             <Bar data={data} />
         </div>
     );
