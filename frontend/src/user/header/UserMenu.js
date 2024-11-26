3a4b8be31f7b77be4913ec0fbfda15854b3d07bf
Austine <chucky891213@naver.com>
2024-11-26 11:58:59 +0900

    MR: 겹치는 소스 코드 정리

--- frontend/src/user/header/UserMenu.js
+++ frontend/src/user/header/UserMenu.js
@@ -2,14 +2,10 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HeaderMenu from 'user/header/HeaderMenu';
 import ContentHome from 'user/content/ContentHome';
-<<<<<<< HEAD
 import UserLogin from 'user/content/UserLogin';
 import UserMemberShip from 'user/content/UserMemberShip';
 import NaverCallback from 'user/content/NaverCallback';
 import KakaoCallback from 'user/content/KakaoCallback';
-=======
-// import UserLogin from 'user/content/UserLogin';
->>>>>>> b46ef0b23b7c4a1207425175a90e9532ada967ad
 import FooterMain from 'user/footer/FooterMain';
 import ReservationPage from 'reservations/ReservationPage';
 import ReservationDetail from 'reservations/ReservationDetail';
@@ -23,16 +19,12 @@ const UserMenu = () => {
       <div className='user-menu-content-wrap'>
         <Routes>
           <Route path="/" element={<ContentHome />} ></Route>
-<<<<<<< HEAD
           <Route path="/login" element={<UserLogin />} ></Route>
           <Route path="/membership" element={<UserMemberShip />} ></Route>
           <Route path="/naver-callback" element={<NaverCallback />} /> 
           <Route path="/kakao-callback" element={<KakaoCallback />} /> 
           <Route path="/reservation" element={<ReservationPage />} /> 
           <Route path="/reservationdetail" element={<ReservationDetail />} /> 
-=======
-          {/* <Route path="/login" element={<UserLogin />} ></Route> */}
->>>>>>> b46ef0b23b7c4a1207425175a90e9532ada967ad
         </Routes>
       </div>
       <div className='user-menu-footer-wrap'>
