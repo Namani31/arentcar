26f4926f1f7016c7ca791a8c94d5faf3daee5118
Austine <chucky891213@naver.com>
2024-11-26 11:42:03 +0900

    MR: 겹치는 소스 코드 정리

--- backend/src/main/java/com/apple/arentcar/model/RentalCars.java
+++ backend/src/main/java/com/apple/arentcar/model/RentalCars.java
@@ -4,17 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-<<<<<<< HEAD
 import java.time.LocalDateTime;
 
-=======
->>>>>>> f8fc7f67076a81edcc716051e5aefbe2fa95984a
 @Getter
 @Setter
 @NoArgsConstructor
 public class RentalCars {
-
-<<<<<<< HEAD
+    
     private Integer carCode;
     private Integer carTypeCode;
     private String carNumber;
@@ -25,25 +21,4 @@ public class RentalCars {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-=======
-    private String carTypeName;
-
-    private String fuelType;
-
-    private String seatingCapacity;
-
-    private String modelYear;
-
-    private String carImageName;
-
-    private String carManufacturer;
-
-    private String branchName;
-
-    private String brandImageName;
-
-    private String branchLatitude;
-
-    private String branchLongitude;
->>>>>>> f8fc7f67076a81edcc716051e5aefbe2fa95984a
 }
