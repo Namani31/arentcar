08fb7f99306f49c3d22b7099d418b49494ffbf3c
Namani <kwg531@naver.com>
2024-12-03 10:40:20 +0900

    Refactor: Chart와 관련된 RentalCars의 이름 변경

--- backend/src/main/java/com/apple/arentcar/service/RentalCarsChartService.java
+++ backend/src/main/java/com/apple/arentcar/service/RentalCarsChartService.java
@@ -1,19 +1,19 @@
 package com.apple.arentcar.service;
 
 import com.apple.arentcar.dto.RentalCarRankingDataDTO;
-import com.apple.arentcar.mapper.RentalCarsMapper;
+import com.apple.arentcar.mapper.RentalCarsChartMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class RentalCarsService {
+public class RentalCarsChartService {
 
     @Autowired
-    private RentalCarsMapper rentalCarsMapper;
+    private RentalCarsChartMapper rentalCarsChartMapper;
 
     public List<RentalCarRankingDataDTO> getTop5ReservedCars(String startDate, String endDate) {
-        return rentalCarsMapper.getTop5ReservedCars(startDate, endDate);
+        return rentalCarsChartMapper.getTop5ReservedCars(startDate, endDate);
     }
 }
